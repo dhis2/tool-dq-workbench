6ee954a370217b1f6196c4a1defc1a63eb4dd97b
Jason Pickering <jason@dhis2.org>
2025-08-26 08:16:17 +0200

    Fix bug with min/max stage deletion

--- app/web/routes/delete_minmax_stage.py
+++ app/web/routes/delete_minmax_stage.py
@@ -8,11 +8,12 @@ from app.web.routes.api import api_bp
 @api_bp.route('/delete-minmax-stage/<int:stage_index>', methods=['POST'], endpoint ='delete_minmax_stage')
 def delete_minmax_stage_view(stage_index):
     config_path = current_app.config['CONFIG_PATH']
-    config = ConfigManager(config_path, config=None, validate_structure=True, validate_runtime=False).config
+    cm = ConfigManager(config_path, config=None, validate_structure=True, validate_runtime=False)
+    config = cm.config
 
     if 0 <= stage_index < len(config['min_max_stages']):
         removed_stage = config['min_max_stages'].pop(stage_index)
-        save_config(config_path, config)
+        cm.save(config_path)
         flash(f"Deleted min max stage: {removed_stage['name']}", 'success')
     else:
         flash("Invalid stage index", 'danger')
